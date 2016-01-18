275bdf64aa957aabc59dde21ee8580c16a6cb3aa
algobardo <mezzetti@gmail.com>
2016-01-18 17:09:35 +0100

    Desugared

--- example/basic/statsdart.dart
+++ example/basic/statsdart.dart
@@ -5,22 +5,17 @@ import 'package:stats/stats.dart';
 void main() {
   Stats stats = new Stats();
   document.body.append(stats.container);
-
   CanvasElement canvas = new CanvasElement();
   canvas.width = 512;
   canvas.height = 512;
   document.body.append(canvas);
-
   CanvasRenderingContext2D context = canvas.getContext('2d');
   context.fillStyle = 'rgba(127, 0, 255, 0.05)';
-
-  draw (num time) {
+  draw(num time) {
     window.requestAnimationFrame(draw);
     context.clearRect(0, 0, canvas.width, canvas.height);
-
     stats.begin();
     double t = new DateTime.now().millisecondsSinceEpoch * 0.001;
-
     for (int i = 0; i < 2000; i++) {
       double x = cos(t + i * 0.01) * 196 + 256;
       double y = sin(t + i * 0.01234) * 196 + 256;
@@ -30,6 +25,5 @@ void main() {
     }
     stats.end();
   }
-
   window.requestAnimationFrame(draw);
 }
